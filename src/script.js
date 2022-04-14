d6ccb7e53778d76519f2cbd2f299dc0c488aecf5
Wicenta <103618547+Wicenta@users.noreply.github.com>
2022-04-14 05:58:18 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -63,12 +63,8 @@ class TextScramble {
 // ——————————————————————————————————————————————————
 
 const phrases = [
-'RageSawyer Loading..,',
-'RageSawyer Loaded!',
-'Project Loading..',
-'I am 21 years old. l live in Turkey.',
-'actual owner of rageturk.com ',
-'C#, Pawn, C++, Asmbly, HTML5/CSS'];
+'test..,'
+];
 
 const el = document.querySelector('.text');
 const fx = new TextScramble(el);
